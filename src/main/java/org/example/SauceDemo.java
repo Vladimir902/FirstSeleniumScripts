4ece76b82e73613f79e1f65f902b8857dc7a774b
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-09-02 09:51:02 +0200

    Added comments for clarification

--- src/main/java/org/example/SauceDemo.java
+++ src/main/java/org/example/SauceDemo.java
@@ -5,17 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SauceDemo {
     public static void main(String[] args) {
+
+        //accessing the website
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        driver.manage().window().maximize(); //entering full window screen on browser
 
+          //locating elements on the website
           driver.findElement(By.id("user-name"));
           driver.findElement(By.name("password"));
           driver.findElement(By.className("input_error"));
 
+          //Filling password field using xpath expression
 //       WebElement element = driver.findElement(By.xpath("//input[@placeholder='Password']"));
 //       element.sendKeys("Password");
 
+        //Filling second div element using css locator
         //driver.findElement(By.cssSelector(".form_group:nth-of-type(2) .input_error.form_input")).sendKeys("aaa");
         driver.findElement(By.cssSelector("input#user-name")).sendKeys("error_user");
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
