846d4d97ce6790ee22272015a00edbb4b0c6d342
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-09-02 13:48:12 +0200

    Added the automation for DemoQA script

--- src/main/java/org/example/DemoQA.java
+++ src/main/java/org/example/DemoQA.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoQA {
-    public static void main(String[] args) {
-        System.out.println("aa");
+    public static void main(String[] args)  {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/automation-practice-form");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("firstName")).sendKeys("Vladimir");
+        driver.findElement(By.id("lastName")).sendKeys("Malakovski");
+        driver.findElement(By.id("userEmail")).sendKeys("vladicabt@hotmail.com");
+
+        /// Locate the "Male" radio button label using the for attribute
+        WebElement maleGenderLabel = driver.findElement(By.cssSelector("label[for='gender-radio-1']"));
+
+        // Click on the "Male" radio button label
+        maleGenderLabel.click();
+
+        driver.findElement(By.xpath("//input[@id='userNumber']")).sendKeys("0123456789");
+
+
+        WebElement subjectsInput = driver.findElement(By.id("subjectsInput"));
+
+        // Send keys to the Subjects input field
+        subjectsInput.sendKeys("Math");
+        subjectsInput.sendKeys(Keys.ENTER);
+
+        WebElement element = driver.findElement(By.id("currentAddress"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+        driver.findElement(By.xpath("//label[@for='hobbies-checkbox-3']")).click();
+        driver.findElement(By.id("currentAddress")).sendKeys("Kire Kostadinovski 17");
+
+
     }
 }
